66ea262cdd1490ffa4701ff61a300b9262c02e4a
jidicula <johanan@forcepush.tech>
2022-01-29 18:43:47 -0500

    refactor(server): Remove handler maker function

--- main.go
+++ main.go
@@ -25,41 +25,35 @@ import (
 )
 
 func main() {
-
-	ilCh := make(chan checker.IssuerList)
-	handleCheck := makeHandleCheck(ilCh)
-
 	http.HandleFunc("/", handleCheck)
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
 
-func makeHandleCheck(ilCh <-chan checker.IssuerList) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
+func handleCheck(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method != http.MethodGet {
-			w.Header().Set("Allow", http.MethodGet)
-			http.Error(w, "expect method GET at /?iss=<url>", http.StatusMethodNotAllowed)
-			return
-		}
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, "expect method GET at /?iss=<url>", http.StatusMethodNotAllowed)
+		return
+	}
 
-		// serveraddress:port?iss=<url>
-		issURL := r.URL.Query().Get("iss")
-		if issURL == "" {
-			http.Error(w, "No issuer URL provided", http.StatusBadRequest)
-			return
-		}
+	// serveraddress:port?iss=<url>
+	issURL := r.URL.Query().Get("iss")
+	if issURL == "" {
+		http.Error(w, "No issuer URL provided", http.StatusBadRequest)
+		return
+	}
 
-		il, err := checker.NewIssuerList()
-		if err != nil {
-			http.Error(w, "Error retrieving VCI issuer list", http.StatusInternalServerError)
-			return
-		}
-		response := fmt.Sprintf(`{"message": %t}`, il.IsTrusted(issURL))
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		x, err := w.Write([]byte(response))
-		if err != nil {
-			fmt.Printf("%s, %d bytes written", err, x)
-		}
+	il, err := checker.NewIssuerList()
+	if err != nil {
+		http.Error(w, "Error retrieving VCI issuer list", http.StatusInternalServerError)
+		return
+	}
+	response := fmt.Sprintf(`{"message": %t}`, il.IsTrusted(issURL))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	x, err := w.Write([]byte(response))
+	if err != nil {
+		fmt.Printf("%s, %d bytes written", err, x)
 	}
 }
